a48467a0e7f6b414667a36a36c2d47aee3dc6e1a
julien-baumgartner <Julien.Baumgartner@he-arc.ch>
2016-10-19 15:38:13 +0200

    Site avec authentification

---
+++ Site/resources/views/rooms.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-success">
+                <div class="panel-heading">Salles</div>
+
+                      <p> Liste des salles</p>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
