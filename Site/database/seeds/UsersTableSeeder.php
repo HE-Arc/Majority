2ce6063abea51f2c693f446a6ee08799aa15577e
joel-vaucher <joel.vaucher@he-arc.ch>
2016-11-09 14:10:34 +0100

    Seed pour la table User

---
+++ Site/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		for($i = 0; $i < 10; $i++){
+			DB::table('users')->insert([
+				'name' => str_random(10),
+				'email' => str_random(10).'@gmail.com',
+				'password' => bcrypt('secret'),
+				'remember_token' => str_random(100),
+				'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+				'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+			]);
+		}
+    }
+}
