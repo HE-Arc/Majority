9d56c17c72c88e4e0278522faa8f3de6d3527082
julien-baumgartner <julien.baumgartner@he-arc.ch>
2016-12-18 14:24:17 +0100

    correction affichage des sbonnes salles uniquement

--- Site/app/Http/Controllers/RoomController.php
+++ Site/app/Http/Controllers/RoomController.php
@@ -3,53 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 use App\Http\Requests;
 
 use App\Game;
 use App\Participant;
 use App\Round;
+use App\User;
 
 class RoomController extends Controller
 {
     public function show()
     {
-        $participants = Participant::where('user_id', 1)->get();
-        $rounds0 = Round::select("game_id")->where('n_round', 0)->get();
-        $rounds1 = Round::select("game_id")->where('n_round', 1)->get();
-
-        $gameId = [];
-
-        foreach($rounds0 as $existedGame) {
-            $found = 0;
-            foreach($rounds1 as $startedGame){
-                if($existedGame->game_id == $startedGame->game_id){
-                    $found = 1;
-                }
-            }
-            if($found == 0){
-                array_push($gameId, $existedGame->game_id);
-            }
-        }
-        foreach($participants as $participant) {
-            if(!in_array($participant->game_id, $gameId)) {
-                array_push($gameId, $participant->game_id);
-            }
-        }
-
-        $games = Game::whereIn('id', $gameId)->get();
-        //$games = Game::all();
+        $games = Game::all();
+		$rounds1 = Round::where('n_round', 1)->get();
+		
+		$user = User::where('id', Auth::id())->first();
         $data = [];
 
         foreach($games as $game) {
-          $participants = Participant::where('game_id', $game->id)->get();
+			$addRoom = false;
+            $participants = Participant::where('game_id', $game->id)->get();
 
-          $i = 0;
-        //  if($game->max_player > count($participants)){
-          //  $data[$i] = [$game, count($participants)];
-            $i++;
-            array_push($data, [$game, count($participants)]);
-        //  }
+			if($game->max_player > count($participants)){
+				$addRoom = true;
+				foreach($rounds1 as $startedGame){
+					if($game->id == $startedGame->game_id){
+						$addRoom = false;
+						break;
+					}
+				}
+			}
+			
+			foreach($participants as $participant) {
+				if($participant->user_id == $user->id) {
+					$addRoom = true;
+					break;
+				}
+			}
+			
+			if($addRoom){
+				array_push($data, [$game, count($participants)]);
+			}
         }
 
         return view('rooms')->withData($data);
